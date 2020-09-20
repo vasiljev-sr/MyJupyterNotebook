728d8d0c5b2cda9287679b12eeda3019626a545e
Ravil <ravil.vasilev@yandex.ru>
2020-09-20 10:47:40 +0300

    пороботал на проектом Udacity

--- Progects_from_book/10/deck.py
+++ Progects_from_book/10/deck.py
@@ -1,33 +1,57 @@
-import random
+# deck.py
+"""Deck class represents a deck of Cards."""
+import random 
 from card import Card
 
 class DeckOfCards:
-     NUMBER_OF_CARDS = 52
+    NUMBER_OF_CARDS = 52  # constant number of Cards
 
-     def __init__(self):
-        '''Инициализирует колоду'''
-        self._curent_card = 0
+    def __init__(self):
+        """Initialize the deck."""
+        self._current_card = 0
         self._deck = []
 
-        for i in range(DeckOfCards.NUMBER_OF_CARDS):
-            self._deck.append(Card(Card.FACES[i % 13],Card.SUITS[ i //13]))
+        for count in range(DeckOfCards.NUMBER_OF_CARDS):  
+            self._deck.append(Card(Card.FACES[count % 13], 
+                Card.SUITS[count // 13]))
 
-        def shuffle(self):
-            self._curent_card = 0
-            random.shuffle(self._deck)
+    def shuffle(self):
+        """Shuffle deck."""
+        self._current_card = 0
+        random.shuffle(self._deck)    
 
-        def deal_card(self):
-            try:
-                card = self._deck[self._current_card]
-                self._current_card += 1
-                return card
-            except:
-                return None
-        def __str__(self):
-            s = ''
+    def deal_card(self):
+        """Return one Card."""
+        try:
+            card = self._deck[self._current_card]
+            self._current_card += 1
+            return card
+        except:
+            return None  
 
-            for i, card in enumerate(self._deck):
-                s += f'{self._deck[i]:<19}'
-                if i % 4 == 0:
-                    s += '\n'
-            return s
+    def __str__(self):
+        """Return a string representation of the current _deck."""
+        s = ''
+
+        for index, card in enumerate(self._deck):
+            s += f'{self._deck[index]:<19}'
+            if (index + 1) % 4 == 0:
+                s += '\n'
+        
+        return s
+
+
+##########################################################################
+# (C) Copyright 2019 by Deitel & Associates, Inc. and                    #
+# Pearson Education, Inc. All Rights Reserved.                           #
+#                                                                        #
+# DISCLAIMER: The authors and publisher of this book have used their     #
+# best efforts in preparing the book. These efforts include the          #
+# development, research, and testing of the theories and programs        #
+# to determine their effectiveness. The authors and publisher make       #
+# no warranty of any kind, expressed or implied, with regard to these    #
+# programs or to the documentation contained in these books. The authors #
+# and publisher shall not be liable in any event for incidental or       #
+# consequential damages in connection with, or arising out of, the       #
+# furnishing, performance, or use of these programs.                     #
+##########################################################################
